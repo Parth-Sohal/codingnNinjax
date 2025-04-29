8c95c4ac17f358652bdebd94880383184aaa8515
Naman Sharma <namansharma2005206@gmail.com>
2025-04-29 21:38:02 +0530

    updated the file location and moved the css part in css file and created a new file of js

---
+++ google.js
@@ -0,0 +1,4 @@
+// Add functionality to the "I'm Feeling Lucky" button
+document.getElementById('lucky-button').addEventListener('click', function () {
+    window.location.href = 'https://www.google.com/doodles';
+});
